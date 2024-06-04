be6fa94c8ea8c5c50a27c9de75422b90d30b9cf0
Susizhou <susanachen2000@gmail.com>
2024-06-04 17:28:23 +0100

    update provider

--- terraform/remote-state/provider.tf
+++ terraform/remote-state/provider.tf
@@ -3,5 +3,11 @@ provider "aws" {
 }
 
 terraform {
-  required_version = ">= 1.0.0, < 2.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  required_version = ">=1.4.4"
 }
